dfa5a69484eff60568e8d18ae5a42116fbb30a83
downkhg <downkhg@gmail.com>
2022-07-28 13:14:12 +0900

    GUI레이아웃 구성하기

---
+++ GameEngineProgramming/Project/GDTC2022-1/Assets/Scripts/GUI/GUIPlayerInfo.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIPlayerInfo : MonoBehaviour
+{
+    public Text textName;
+    public Text textLv;
+    public GUIStatusBar guiHP;
+
+    void Set(string name, int lv)
+    {
+        textName.text = name;
+        textLv.text = string.Format("Lv.{1}", lv);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set("test",99);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GameEngineProgramming/Project/GDTC2022-1/Assets/Scripts/GUI/GUIStatusBar.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIStatusBar : MonoBehaviour
+{
+    public Text textLable;
+    public RectTransform rectBar;
+    public RectTransform rectBarBG;
+    public Color colorBar = Color.white;
+
+    void Set(string labletext, Color color, int state, int state_max)
+    {
+        textLable.text = labletext;
+        //Image img = rectBar.gameObject.GetComponent<Image>();
+        //img.color = color;
+        rectBar.GetComponent<Image>().color = color;
+        SetState(state, state_max);
+    }
+
+    void SetState(float state, float state_max)
+    {
+        float fStateRat = state / state_max;
+        Vector2 vMaxSize = rectBarBG.sizeDelta;
+        Vector2 vBarSize = rectBar.sizeDelta;
+        vBarSize.x = vMaxSize.x * fStateRat;
+        rectBar.sizeDelta = vBarSize;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set("text", colorBar, 10, 100);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
