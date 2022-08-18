ed50c67bec0bb46a810d45215e4201ac8b4edf89
downkhg <downkhg@gmail.com>
2022-08-18 13:09:10 +0900

    2D애니메이션 복습

--- GameClientProgramming/AnimationTestProject/Assets/Scripts/AnimationController.cs
+++ GameClientProgramming/AnimationTestProject/Assets/Scripts/AnimationController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour
 {
     public Animator animator;
+    public float Speed = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +17,10 @@ public class AnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
+        if(Input.GetKey(KeyCode.A))
         {
             animator.SetInteger("SetState", 1);
+            transform.position += Vector3.left * Speed * Time.deltaTime;
             transform.localScale = new Vector3(-1,1,1);
         }
         if (Input.GetKeyUp(KeyCode.A))
@@ -26,9 +28,10 @@ public class AnimationController : MonoBehaviour
             animator.SetInteger("SetState", 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             animator.SetInteger("SetState", 1);
+            transform.position += Vector3.right * Speed * Time.deltaTime;
             transform.localScale = new Vector3(1, 1, 1);
         }
         if (Input.GetKeyUp(KeyCode.D))
@@ -36,8 +39,13 @@ public class AnimationController : MonoBehaviour
             animator.SetInteger("SetState", 0);
         }
 
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space");
+            animator.SetTrigger("hurt");
+        }
     }
-
+     
     private void OnGUI()
     {
         AnimatorClipInfo[] aniClipinfo = animator.GetCurrentAnimatorClipInfo(0);
@@ -45,5 +53,21 @@ public class AnimationController : MonoBehaviour
 
         string log = string.Format("{0}:{1}", aniClipinfo[0].clip.name, aniStatusInfo.normalizedTime);
         GUI.Box(new Rect(0, 0, 500, 20), log);
+
+        if (aniClipinfo[0].clip.name == "hurt")
+        {
+            Debug.Log("hurt!!!!");
+            if (aniStatusInfo.normalizedTime >= 1)
+            {
+                Debug.Log("hurt end!");
+                animator.SetInteger("SetState", 0);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("OnCollisionEnter2D:"+collision.gameObject.name);
+        //animator.SetInteger("SetState", 4);   
     }
 }
