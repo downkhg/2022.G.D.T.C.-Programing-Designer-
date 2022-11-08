ff7fa0268946cdeb9b0f8ff189700728907ce8af
downkhg <downkhg@gmail.com>
2022-11-08 10:26:20 +0900

    UDPTCP콘솔실행파일

---
+++ GameNetworkingProgramming/CSSample/UdpChatClient/UdpChatClient/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpChatClient
+{
+    class Program
+    {
+        static int port = 11000;
+        static void SendMsg(string ip, string strSendMsg)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            Console.WriteLine("Send:" + strSendMsg);
+            IPAddress broadcast = IPAddress.Parse(ip);
+
+            byte[] sendbuf = Encoding.ASCII.GetBytes(strSendMsg);
+            IPEndPoint ep = new IPEndPoint(broadcast, port);
+
+            s.SendTo(sendbuf, ep);
+
+            Console.WriteLine("Message sent to the broadcast address");
+        }
+
+        public static IPAddress GetIPAddress()
+        {
+            IPHostEntry he = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress iPAddress = null;
+            // 처음으로 발견되는 ipv4 주소를 사용한다.
+            foreach (IPAddress addr in he.AddressList)
+            {
+                if (addr.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    iPAddress = addr;
+                    break;
+                }
+            }
+
+            // 주소가 없다면..
+            if (iPAddress == null)
+                // 로컬호스트 주소를 사용한다.
+                iPAddress = IPAddress.Loopback;
+
+            return iPAddress;
+        }
+
+        static void Main(string[] args)
+        {
+            string ip = GetIPAddress().ToString();
+            Console.WriteLine("IP:" + ip);
+            SendMsg("127.0.0.1", "connect");
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+                string msg = string.Format("{0};{1}",ip,input);
+                SendMsg("192.168.1.49", msg);
+            }
+        }
+    }
+}
---
+++ GameNetworkingProgramming/CSSample/UdpServer/UdpServer/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpServer
+{
+    class Program
+    {
+        static int port = 11000;
+        static void SendMsg(string ip, string strSendMsg)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            Console.WriteLine("Send:" + strSendMsg);
+            IPAddress broadcast = IPAddress.Parse(ip);
+
+            byte[] sendbuf = Encoding.ASCII.GetBytes(strSendMsg);
+            IPEndPoint ep = new IPEndPoint(broadcast, port);
+
+            s.SendTo(sendbuf, ep);
+
+            Console.WriteLine("Message sent to the broadcast address");
+        }
+        static void Main(string[] args)
+        {
+            int listenPort = 11000;
+            List<string> clientIps = new List<string>();
+            UdpClient listener = new UdpClient(listenPort);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref groupEP);
+                    string msg = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+                    Console.WriteLine($"Received broadcast from {groupEP} :");
+                    Console.WriteLine($" {msg}");
+                    
+                    
+                    string[] strSplit = msg.Split(';');
+                    if (strSplit.Length > 0)
+                    {
+                        string header = strSplit[0];
+                        string content = strSplit[1];
+               
+
+                        if (header == "connect")
+                        {
+                            clientIps.Add(content);
+                            Console.WriteLine("Connect:"+ content);
+                        }
+                        else
+                        {
+                            foreach (string ip in clientIps)
+                            {
+                                if(ip != header) SendMsg(ip, msg);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+    }
+}
